27e52a13acaaf0093fb1b4c15f72ea203e450387
gamach1 <154212189+gamach1@users.noreply.github.com>
2024-02-16 11:02:28 +0300

    Add files via upload

---
+++ program/algr16.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int F(int n)
+{
+    if (n >= 2025)
+    {
+        return n;
+    }
+    else
+    {
+        return n + F(n + 2);
+    }
+}
+
+int main()
+{
+    int result = F(2022) - F(2023);
+    cout << result << endl;
+}
---
+++ program/algr17.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    ifstream f("17.txt");
+    int count = 0;
+    int maxsum = 0;
+    int arr[10000];
+
+    for (int i = 0; i < 10000; i++)
+    {
+        f >> arr[i];
+    }
+
+    for (int i = 0; i < 9999; i++)
+    {
+        for (int j = i + 1; j < 10000; j++)
+        {
+            if ((arr[i] * arr[j]) % 10 == 0)
+            {
+                count++;
+            }
+            if (arr[i] + arr[j] > maxsum)
+            {
+                maxsum = arr[i] + arr[j];
+            }
+        }
+    }
+    cout << count << " " << maxsum << endl;
+    return 0;
+}
